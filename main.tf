32f4217bd084be66c67a8603ecb763cce853c0e0
ptonini <pedro.tonini@hotmail.com>
2023-12-06 21:55:34 -0300

    feat: refactored inputs

--- main.tf
+++ main.tf
@@ -1,38 +1,25 @@
-locals {
-  builtin_actions = {
-    redirect_to_https = {
-      type = "redirect"
-      options = {
-        port     = "443"
-        protocol = "HTTPS"
-      }
-    }
-  }
-  selected_builtin_actions_count = length(var.builtin_actions)
-  selected_builtin_actions       = { for l in var.builtin_actions : (1 + index(var.builtin_actions, l)) => local.builtin_actions[l] }
-  extra_actions                  = { for k, v in var.actions : (k + local.selected_builtin_actions_count) => v }
-}
-
 resource "aws_lb_listener" "this" {
   load_balancer_arn = var.load_balancer.arn
   port              = var.port
   protocol          = var.protocol
-  certificate_arn   = var.certificate == null ? null : var.certificate.arn
+  certificate_arn   = var.certificate.arn
+
   dynamic "default_action" {
-    for_each = merge(local.selected_builtin_actions, local.extra_actions)
+    for_each = var.actions
     content {
       order            = default_action.key
-      type             = default_action.value["type"]
-      target_group_arn = default_action.value["type"] == "forward" ? default_action.value["target_group_arn"] : null
+      type             = default_action.value.type
+      target_group_arn = default_action.value.type == "forward" ? default_action.value.target_group_arn : null
+
       dynamic "redirect" {
-        for_each = default_action.value["type"] == "redirect" ? { 0 = default_action.value["options"] } : {}
+        for_each = default_action.value.redirect_options[*]
         content {
-          host        = try(redirect.value["host"], null)
-          path        = try(redirect.value["path"], null)
-          port        = try(redirect.value["port"], null)
-          protocol    = try(redirect.value["protocol"], null)
-          query       = try(redirect.value["query"], null)
-          status_code = try(redirect.value["status_code"], "HTTP_301")
+          host        = redirect.value.host
+          path        = redirect.value.path
+          port        = redirect.value.port
+          protocol    = redirect.value.protocol
+          query       = redirect.value.query
+          status_code = redirect.value.status_code
         }
       }
     }
@@ -43,27 +30,31 @@ resource "aws_lb_listener_rule" "this" {
   for_each     = var.rules
   priority     = each.key
   listener_arn = aws_lb_listener.this.arn
+
   action {
-    type = each.value["type"]
+    type = each.value.type
+
     dynamic "redirect" {
-      for_each = each.value["type"] == "redirect" ? { 0 = each.value["options"] } : {}
+      for_each = each.value.redirect_options[*]
       content {
-        host        = try(redirect.value["host"], null)
-        path        = try(redirect.value["path"], null)
-        port        = try(redirect.value["port"], null)
-        protocol    = try(redirect.value["protocol"], null)
-        query       = try(redirect.value["query"], null)
-        status_code = try(redirect.value["status_code"], "HTTP_301")
+        host        = redirect.value.host
+        path        = redirect.value.path
+        port        = redirect.value.port
+        protocol    = redirect.value.protocol
+        query       = redirect.value.query
+        status_code = redirect.value.status_code
       }
     }
   }
+
   dynamic "condition" {
-    for_each = each.value["conditions"]
+    for_each = each.value.conditions
     content {
+
       dynamic "host_header" {
-        for_each = try(condition.value["host_header"]) != null ? { 0 = condition.value["host_header"] } : {}
+        for_each = condition.value.host_header != null ? { 0 = condition.value.host_header } : {}
         content {
-          values = condition.value["host_header"]
+          values = condition.value.host_header
         }
       }
     }
--- variables.tf
+++ variables.tf
@@ -5,36 +5,52 @@ variable "load_balancer" {
 }
 
 variable "port" {
-  type     = number
-  default  = "80"
-  nullable = false
+  type    = number
+  default = "80"
 }
 
 variable "protocol" {
-  type     = string
-  default  = "HTTP"
-  nullable = false
-
+  type    = string
+  default = "HTTP"
 }
 
 variable "certificate" {
   type = object({
-    arn = string
+    arn = optional(string)
   })
-  default = {
-    arn = null
-  }
-}
-
-variable "actions" {
   default = {}
 }
 
-variable "builtin_actions" {
-  type    = list(string)
-  default = []
+variable "actions" {
+  type = map(object({
+    type             = string
+    target_group_arn = optional(string)
+    redirect_options = optional(object({
+      host        = optional(string)
+      path        = optional(string)
+      port        = optional(string)
+      protocol    = optional(string)
+      query       = optional(string)
+      status_code = optional(string, "HTTP_301")
+    }))
+  }))
+  default = {}
 }
 
 variable "rules" {
+  type = map(object({
+    type = string
+    redirect_options = optional(object({
+      host        = optional(string)
+      path        = optional(string)
+      port        = optional(string)
+      protocol    = optional(string)
+      query       = optional(string)
+      status_code = optional(string, "HTTP_301")
+    }))
+    conditions = map(object({
+      host_header = optional(string)
+    }))
+  }))
   default = {}
 }
